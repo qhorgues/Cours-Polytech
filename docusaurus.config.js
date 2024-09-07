c0354604fe8c8228f0f742870b9bafb0ad032f61
Quentin Horgues <quentin.horgues@outlook.fr>
2024-09-08 01:19:23 +0200

    correction base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,12 +19,12 @@ export default {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Cours-Polytech/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Quentin Horgues', // Usually your GitHub org/user name.
+  projectName: 'Cours Polytech', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
