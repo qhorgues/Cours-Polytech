cb0a472d55e0e8ea4aec9b2ac89a05ba68bea828
Quentin Horgues <quentin.horgues@outlook.fr>
2024-09-08 01:32:25 +0200

    correction

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ export default {
   url: 'https://qhorgues.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Cours-Polytech/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
