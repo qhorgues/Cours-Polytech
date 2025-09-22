bc8662866d75f36202cb0a00e6ce352d9a7400b6
qhorgues <quentin.horgues@outlook.fr>
2025-09-22 07:55:39 +0200

    gestion-projet

--- src/components/HomepageSubjects/index.js
+++ src/components/HomepageSubjects/index.js
@@ -1,59 +1,60 @@
-import React from 'react';
-import Link from '@docusaurus/Link';
+import React from "react";
+import Link from "@docusaurus/Link";
 
 const ListSubject = [
   {
-    id: 'maths',
-    title: 'Mathématiques',
-    Svg: require('@site/static/img/math-ico.svg').default,
-    link: '/docs/category/ensemble-des-chapitres',
+    id: "analyses-de-donnees",
+    title: "Analyses de données",
+    Svg: require("@site/static/img/data-analysis.svg").default,
+    link: "/docs/category/1-maths",
   },
   {
-    id: 'info',
-    title: 'Programmation',
-    Svg: require('@site/static/img/prog-ico.svg').default,
-    link: '/docs/category/ensemble-des-langagues-de-programmation',
+    id: "maths",
+    title: "Mathématiques",
+    Svg: require("@site/static/img/math-ico.svg").default,
+    link: "/docs/category/ensemble-des-chapitres",
   },
   {
-    id: 'algo',
-    title: 'Algorithmique',
-    Svg: require('@site/static/img/algo-ico.svg').default,
-    link: '/docs/category/ensemble-des-chapitres-1',
+    id: "info",
+    title: "Programmation",
+    Svg: require("@site/static/img/prog-ico.svg").default,
+    link: "/docs/category/ensemble-des-langagues-de-programmation",
   },
   {
-    id: 'archi_os',
-    title: 'Architecture & OS',
-    Svg: require('@site/static/img/archi-ico.svg').default,
-    link: '/docs/category/ensemble-des-chapitres-2',
+    id: "algo",
+    title: "Algorithmique",
+    Svg: require("@site/static/img/algo-ico.svg").default,
+    link: "/docs/category/ensemble-des-chapitres-1",
   },
   {
-    id: 'en',
-    title: 'Anglais',
-    Svg: require('@site/static/img/en-ico.svg').default,
-    link: '/docs/category/all-english-chapters',
+    id: "archi_os",
+    title: "Architecture & OS",
+    Svg: require("@site/static/img/archi-ico.svg").default,
+    link: "/docs/category/ensemble-des-chapitres-2",
   },
   {
-    id: 'reseau',
-    title: 'Réseaux',
-    Svg: require('@site/static/img/reseaux-ico.svg').default,
-    link: '/docs/category/ensemble-des-chapitres-3',
+    id: "en",
+    title: "Anglais",
+    Svg: require("@site/static/img/en-ico.svg").default,
+    link: "/docs/category/all-english-chapters",
+  },
+  {
+    id: "reseau",
+    title: "Réseaux",
+    Svg: require("@site/static/img/reseaux-ico.svg").default,
+    link: "/docs/category/ensemble-des-chapitres-3",
   },
 ];
 
-
 function Subject({ id, Svg, title, link }) {
-
   return (
     <article className="col col--6 margin-bottom--lg">
-      <Link className="card padding--lg card-img-button"
-        to={link}>
-          <div className='scaling-svg-container'>
-            <Svg id={id} role="img" className="img-button"/>
-          </div>
+      <Link className="card padding--lg card-img-button" to={link}>
+        <div className="scaling-svg-container">
+          <Svg id={id} role="img" className="img-button" />
+        </div>
         <div className="text-img-button">
-          <h2 className="text--truncate">
-            {title}
-          </h2>
+          <h2 className="text--truncate">{title}</h2>
         </div>
       </Link>
     </article>
--- src/pages/gestion-de-projet.md
+++ src/pages/gestion-de-projet.md
@@ -25,3 +25,40 @@
 * Warrantly
 -> Expiration of the contract
 * Mainteance
+
+## Workload
+* Unité: man-day (1 m/d = 7 hours), man-mounth (1 m/mth = 20 m/d)
+* Used to obtain a projected cose (C x salary)
+* Project size
+  - $C \lt 6\ m/mth$: very small project
+  - $6\ m/mth \lt C \lt 12\ m/mth$ : small
+  - $12\ m/mth \lt C \lt 30\ m/mth$ : medium
+  - $30\ m/mth \lt C \lt 100\ m/mth$ : large
+  - $C \gt 100$ : very large
+
+* Workload (m/mth) = $a \times (kLoC)^b$
+* Duration (months) = $c(workload)^d$
+
+* Workload distribution
+  - RFP: not included
+  - Specifications: $10\%$
+  - Design $21\%$
+  - Realization + Unit Testin $47\%$
+  - ...
+
+## Risks management
+* Risk
+  - Probable (**unproven**) envent with adverse consequences
+    + **identifiable**
+    + **quantifiable** in terms **of probability and impact**
+    + **preventivee actions are possible**
+  - Problem: proven event (often a risk)
+    + Managed throught corrective **actions**
+  - Contingency (aléa): an event **that has occured** but **was not foreseeable**
+
+* 5 steps
+  - Identify Risks
+  - Assess their impact on: costs; deadlines; quality
+  - Define actions to reduce unacceptable risks
+  - Monitor actions
+  - ...
